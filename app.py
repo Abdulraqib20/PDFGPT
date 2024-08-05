a860213677cc62ace03e671f8af41721925490cc
raqibcodes <abdulraqibshakir03@gmail.com>
2024-08-05 22:52:18 +0100

    Update app.py

--- app.py
+++ app.py
@@ -247,8 +247,8 @@ components.html(custom_html, height=460, scrolling=True)
 # PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
 # GROQ_API_KEY=os.getenv("GROQ_API_KEY")
 
-PINECONE_API_KEY = st.secrets("PINECONE_API_KEY")
-GROQ_API_KEY = st.secrets("GROQ_API_KEY")
+PINECONE_API_KEY = st.secrets["PINECONE_API_KEY"]
+GROQ_API_KEY = st.secrets["GROQ_API_KEY"]
 
 
 OPENAI_API_KEY=os.getenv("OPENAI_API_KEY")
