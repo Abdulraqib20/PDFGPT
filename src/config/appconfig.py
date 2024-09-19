6105b96ed305810bf82b6fa2f2500a57d17403d1
raqibcodes <abdulraqibshakir03@gmail.com>
2024-09-19 14:38:03 +0100

    config files updated

--- src/config/appconfig.py
+++ src/config/appconfig.py
@@ -5,8 +5,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Retrieve the environment variables (Local)
-GROQ_API_KEY = os.getenv("GROQ_API_KEY")
-PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
+# GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+# PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
 
 # Retrieve the environment variables (Streamlit Cloud)
 GROQ_API_KEY = st.secrets["GROQ_API_KEY"]
